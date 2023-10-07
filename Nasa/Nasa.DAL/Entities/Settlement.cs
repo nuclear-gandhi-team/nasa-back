4d4facfbc7f4d51546374a7cf31b17b5d5d3fa56
VladTkach <vladand2003@gmail.com>
2023-10-07 13:22:33 +0300

    added entities

---
+++ Nasa/Nasa.DAL/Entities/Settlement.cs
@@ -0,0 +1,9 @@
+﻿using Nasa.DAL.Entities.Common;
+
+namespace Nasa.DAL.Entities;
+
+public class Settlement: BaseEntity<int>
+{
+    public string Name { get; set; } = string.Empty;
+    public string Сoordinates { get; set; } = string.Empty;
+}
---
+++ Nasa/Nasa.DAL/Entities/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using Nasa.DAL.Entities.Common;
+
+namespace Nasa.DAL.Entities;
+
+public class Subscription: BaseEntity<int>
+{
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
+
+    public int SettlementId { get; set; }
+
+    public Settlement Settlement { get; set; } = null!;
+}
