1795e3b8e7f9e829f2dda0b4aa02fed38e84080e
VladTkach <vladand2003@gmail.com>
2023-10-07 12:06:33 +0300

    initial create

---
+++ Nasa/Nasa.API/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace Nasa.API;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddControllers();
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors(builder => builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(cfg =>
+        {
+            cfg.MapControllers();
+        });
+    }
+}
