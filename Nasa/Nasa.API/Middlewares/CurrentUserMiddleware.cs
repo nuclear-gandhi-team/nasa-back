637d24d73c3247bb825d47decec91319bcfa20e2
VladTkach <vladand2003@gmail.com>
2023-10-08 13:45:16 +0300

    added user id getter

---
+++ Nasa/Nasa.API/Middlewares/CurrentUserMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using Nasa.BLL.ServicesContracts;
+
+namespace Nasa.API.Middlewares;
+
+public class CurrentUserMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CurrentUserMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, IUserIdSetter userIdSetter)
+    {
+        Console.WriteLine(context.User.Claims);
+        var id = context.User.Claims.FirstOrDefault()?.Value;
+
+        if (id is not null && int.TryParse(id, out var userId))
+        {
+            userIdSetter.SetCurrentUserId(userId);
+        }
+
+        await _next.Invoke(context);
+    }
+}
